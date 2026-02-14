4e3b79c86c0bfcd75bb5dba7826f35d24a8e863b
crossSiteKikyo <patmailaddress01@gmail.com>
2026-02-14 18:00:39 +0900

    build js css이름 고정

--- test.user.js
+++ test.user.js
@@ -15,8 +15,8 @@
   const supabaseUrl = "";
   const supabaseKey = "";
   // const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
-  const GITHUB_BASE =
-    "https://cdn.jsdelivr.net/gh/crossSiteKikyo/sand_hamster@main/dist/";
+  const GithubHack_base =
+    "https://raw.githack.com/crossSiteKikyo/sand_hamster/main/dist/";
 
   function Sleep(ms) {
     return new Promise(function (resolve, reject) {
@@ -35,10 +35,24 @@
       );
       const responseText = await response.text();
       console.log(responseText);
-      document.writeln(responseText);
+      document.documentElement.innerHTML = responseText;
+
+      // 2. CSS 주입
+      const link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.crossOrigin = "anonymous";
+      link.href = `${GithubHack_base}assets/index.css`;
+      document.head.appendChild(link);
+
+      // JS 주입 (type="module" 설정 필수)
+      const script = document.createElement("script");
+      script.type = "module";
+      script.crossOrigin = "anonymous";
+      script.src = `${GithubHack_base}assets/index.js`;
+      document.head.appendChild(script);
     },
   };
-  HtmlManager.changeHtml();
+  // HtmlManager.changeHtml();
   // ui 추가
-  GM_registerMenuCommand("ghghghghghg호호호", () => {});
+  GM_registerMenuCommand("html 변경", HtmlManager.changeHtml);
 })();
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/index.js`,
+        chunkFileNames: `assets/index.js`,
+        assetFileNames: `assets/index.css`,
+      },
+    },
+  },
 });
